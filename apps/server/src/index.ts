0b1680ddd9b9ac747bc1fd38e21182a1fa9b72a1
cojoclaudiu <cojoclaudiu@gmail.com>
2026-03-10 00:37:54 +0000

    add data structure for songs to the server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,15 +1,26 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const songs = [
+  { id: 1, name: "Flowers", author: "Miley Cyrus", progress: 0.15 },
+  { id: 2, name: "Anti-Hero", author: "Taylor Swift", progress: 0.27 },
+  { id: 3, name: "As It Was", author: "Harry Styles", progress: 0.12 },
+  { id: 4, name: "Heat Waves", author: "Glass Animals", progress: 0.38 },
+  { id: 5, name: "Unholy", author: "Sam Smith ft. Kim Petras", progress: 0.03 },
+];
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get("/songs", (c) => {
+  return c.json(songs);
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
