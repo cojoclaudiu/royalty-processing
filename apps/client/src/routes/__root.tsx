465ed5d4d0fcca6142cd540a3e54f4f2bcd593e6
cojoclaudiu <cojoclaudiu@gmail.com>
2026-03-10 00:42:58 +0000

    cleanup tanstack boilerplate

--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -5,8 +5,6 @@ import {
 } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
 
 import TanStackQueryProvider from '../integrations/tanstack-query/root-provider'
 
@@ -50,11 +48,9 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <head>
         <HeadContent />
       </head>
-      <body className="font-sans antialiased [overflow-wrap:anywhere] selection:bg-[rgba(79,184,178,0.24)]">
+      <body>
         <TanStackQueryProvider>
-          <Header />
           {children}
-          <Footer />
           <TanStackDevtools
             config={{
               position: 'bottom-right',
